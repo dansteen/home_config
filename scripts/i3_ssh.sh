233660f4ecde44b7e7d0243e6c77ce57a527800e
Daniel Steen <dsteen@millennialmedia.com>
2016-01-11 10:09:47 -0500

    fixed tmux for use on remote boxes

--- scripts/i3_ssh.sh
+++ scripts/i3_ssh.sh
@@ -3,4 +3,4 @@
 # this script will wrap ssh for i3 so that we can provide a proper title to the window
 
 NAME=$1
-st -e tmux -c "printf '\033k$NAME\033\\' && ssh -X $NAME" 
+st -e tmux new-session -d "printf '\033k$NAME\033\\' && ssh -X $NAME" \; attach
