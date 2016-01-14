6b2554320a86cdbf3744c0ba83c3978c00eb81c7
Daniel Steen <dsteen@millennialmedia.com>
2016-01-14 15:24:59 -0500

    config changes

--- scripts/i3_ssh.sh
+++ scripts/i3_ssh.sh
@@ -3,4 +3,5 @@
 # this script will wrap ssh for i3 so that we can provide a proper title to the window
 
 NAME=$1
-st -e tmux new-session -d "printf '\033k$NAME\033\\' && ssh -X $NAME" \; attach
+st -e tmux new-session -n $NAME ssh -X $NAME \; attach
+
