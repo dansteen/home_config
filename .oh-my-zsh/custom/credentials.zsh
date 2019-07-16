4af8e7400b32f6b0bcb22043aa00e22368ea85f2
Daniel Steen <dan@steen.blue>
2019-07-16 15:13:21 -0400

    updates

--- .oh-my-zsh/custom/credentials.zsh
+++ .oh-my-zsh/custom/credentials.zsh
@@ -6,7 +6,6 @@ export AWS_SECRET_ACCESS_KEY=`grep aws_secret_access_key ~/.aws/config | awk '{p
 export AWS_DEFAULT_REGION="us-east-1"
 export TF_VAR_aws_access_key="$AWS_ACCESS_KEY_ID"
 export TF_VAR_aws_secret_key="$AWS_SECRET_ACCESS_KEY"
-export TF_VAR_consul_token=`grep consul_token ~/.terraform_config| awk '{print $3}'`
 export TF_VAR_airbrake_key_matching_spark=`grep airbrake_key_matching_spark ~/.terraform_config| awk '{print $3}'`
 export TF_VAR_datadog_api_key=`grep datadog_api_key ~/.terraform_config| awk '{print $3}'`
 export TF_VAR_datadog_app_key=`grep datadog_app_key ~/.terraform_config| awk '{print $3}'`
